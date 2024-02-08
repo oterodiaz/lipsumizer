133195ea0df17a631d4933f3ce4347e70fb3b18d
oterodiaz <44239644+oterodiaz@users.noreply.github.com>
2024-02-08 12:51:31 +0100

    Initial Commit

---
+++ Lipsumizer/LipsumizerApp.swift
@@ -0,0 +1,17 @@
+//
+//  LipsumizerApp.swift
+//  Lipsumizer
+//
+//  Created by Diego Otero on 2024-02-08.
+//
+
+import SwiftUI
+
+@main
+struct LipsumizerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
